23a7a6676c01c7fd6289689120985b7f6f3e15ad
Ramesh Baskarasubramanian <ramesh.baskarasubramanian@live.com>
2020-05-20 20:12:59 +0530

    Added a pageFactory module for providing easy access to the page classes

---
+++ cypress/integration/pageFactory.js
@@ -0,0 +1,7 @@
+import LoginPage from './pages/loginPage';
+import MyGrantsPage from './pages/myGrantsPage';
+import MraApplicationForm from './pages/mraApplicationForm';
+
+export const loginPage = new LoginPage();
+export const myGrantsPage = new MyGrantsPage();
+export const mraApplicationForm = new MraApplicationForm();
